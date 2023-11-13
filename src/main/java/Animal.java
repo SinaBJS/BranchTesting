c83e03c00c760979ed7c45a289b03f27dc24ac74
SinaBJS <sija0001@stud.kea.dk>
2023-11-13 13:07:23 +0100

    Gitignore ændret

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,6 +2,9 @@ public abstract class Animal {
 
     private String name;
 
+    public Animal(String name) {
+        this.name = name;
+    }
 
     public void makeSound(){
     }
--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,4 +1,8 @@
 public class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
+
     @Override
     public void makeSound() {
         System.out.println("Woof");
