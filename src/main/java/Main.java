757cd1f1e2f2755941e5eda904659b5e6e99006b
SinaBJS <sija0001@stud.kea.dk>
2023-11-13 13:25:02 +0100

    Main added & dog and cat objects created

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Labrador");
+        Cat cat1 = new Cat("Mis");
+
+        dog1.makeSound();
+        cat1.makeSound();
+    }
+}
